ac12f918217bcf0c5e344b2f0ecf85934accc4e3
Axel Rantil <axera283@student.liu.se>
2017-09-06 16:02:19 +0200

    c

--- lab1/network.R
+++ lab1/network.R
@@ -16,14 +16,9 @@ View(df)
 
 #bn <- empty.graph(header, num = 5)
 
-bn1 <- hc(df, start = NULL, whitelist = NULL, blacklist = NULL,
-          score = NULL, debug = FALSE, restart = 0, perturb = 1, 
+bn <- hc(df, start = NULL, whitelist = NULL, blacklist = NULL,
+          score = NULL, debug = FALSE, restart = 3, perturb = 1, 
           max.iter = Inf, maxp = Inf, optimized = TRUE)
-
-bn2 <- hc(df, start = NULL, whitelist = NULL, blacklist = NULL,
-          score = NULL, debug = FALSE, restart = 0, perturb = 1, 
-          max.iter = Inf, maxp = Inf, optimized = TRUE)
-  
 
 
 
