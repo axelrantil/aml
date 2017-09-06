735ec7604a03bfdd9107c741e9083fbe5db3e9da
Axel Rantil <axera283@student.liu.se>
2017-09-06 15:42:15 +0200

    hc

--- lab1/import_data.R
+++ lab1/import_data.R
@@ -1,5 +1,54 @@
 load.data <- function() {
-  crx <- read.csv("data/crx.data", header=FALSE)
   
-  return(crx)
+  df <- read.csv("https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data", header = FALSE, sep = " ")
+
+  df$V2 <- as.numeric(df$V2)
+  
+  df$V5 <- as.numeric(df$V5)
+  
+  df$V8 <- as.numeric(df$V8)
+  
+  df$V8 <- df$V8/100
+  
+  df$V11 <- as.numeric(df$V11)
+  
+  df$V13 <- as.numeric(df$V13)
+  
+  df$V16 <- as.numeric(df$V16)
+  
+  df$V18 <- as.numeric(df$V18)
+  
+  df$V21 <- as.factor(df$V21)
+  
+  
+  sapply(df,class)
+  
+  header <- c("Checking account",
+              "Duration",
+              "Credit history",
+              "Purpose",
+              "Amount credit",
+              "Savings account",
+              "Employment since",
+              "Installment rate",
+              "Marital and sex",
+              "Guarantors",
+              "Residence since",
+              "Owned property",
+              "Age",
+              "Installment plans",
+              "Housing",
+              "Current credit here",
+              "Job",
+              "People care for",
+              "Telephone",
+              "Foreign worker",
+              "Good/bad credit"
+              )
+  
+  colnames(df) <- header
+  
+  df <- df[, c("Duration", "Credit history", "Purpose", "Amount credit", "Employment since", "Marital and sex", "Age", "Job", "Housing")]
+  
+  return(list("df"=df, "header"=header))
 }
--- lab1/network.R
+++ lab1/network.R
@@ -1,11 +1,30 @@
-
 library(bnlearn)
 
 source ("import_data.R") 
 
-crx <- load.data()
+ret <- load.data()
 
-crx
+df <- ret$df 
+
+header <- ret$header
+
+sapply(df,class)
+
+#head(df)
+
+View(df)
+
+#bn <- empty.graph(header, num = 5)
+
+bn1 <- hc(df, start = NULL, whitelist = NULL, blacklist = NULL,
+          score = NULL, debug = FALSE, restart = 0, perturb = 1, 
+          max.iter = Inf, maxp = Inf, optimized = TRUE)
+
+bn2 <- hc(df, start = NULL, whitelist = NULL, blacklist = NULL,
+          score = NULL, debug = FALSE, restart = 0, perturb = 1, 
+          max.iter = Inf, maxp = Inf, optimized = TRUE)
+  
 
 
-View(crx)
+
+
