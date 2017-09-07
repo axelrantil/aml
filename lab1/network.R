40cc00dd88917bbe255c9f7fba38df23a7a13403
Axel Rantil <axelrantil@Axels-MacBook-Pro.local>
2017-09-07 09:11:18 +0200

    graphviz

--- lab1/network.R
+++ lab1/network.R
@@ -6,6 +6,8 @@ ret <- load.data()
 
 df <- ret$df 
 
+df2 <- ret$df
+
 header <- ret$header
 
 sapply(df,class)
@@ -17,8 +19,18 @@ View(df)
 #bn <- empty.graph(header, num = 5)
 
 bn <- hc(df, start = NULL, whitelist = NULL, blacklist = NULL,
-          score = NULL, debug = FALSE, restart = 3, perturb = 1, 
+          score = NULL, debug = FALSE, restart = 3, perturb = 3, 
           max.iter = Inf, maxp = Inf, optimized = TRUE)
+
+bn2 <- hc(df2, start = NULL, whitelist = NULL, blacklist = NULL,
+         score = NULL, debug = FALSE, restart = 3, perturb = 3, 
+         max.iter = Inf, maxp = Inf, optimized = TRUE)
+
+graphviz.plot(bn)
+
+all.equal(bn,bn2) # Should be false
+
+
 
 
 
