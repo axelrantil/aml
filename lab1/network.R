2b2cdd68f6bbdc54ba8c90008d8eaa811074d6db
Axel Rantil <axera283@student.liu.se>
2017-09-10 15:02:12 +0200

    task 2

--- lab1/network.R
+++ lab1/network.R
@@ -13,16 +13,13 @@ sapply(df,class)
 
 #View(df)
 
-
-
 bn1 <- hc(df, start = NULL, whitelist = NULL, blacklist = NULL,
-          score = NULL, debug = FALSE, restart = 3, perturb = 3, 
+          score = NULL, debug = FALSE, restart = 0, perturb = 1, 
           max.iter = Inf, maxp = Inf, optimized = TRUE)
 
 bn2 <- hc(df, start = NULL, whitelist = NULL, blacklist = NULL,
-         score = NULL, debug = FALSE, restart = 3, perturb = 3, 
+         score = NULL, debug = FALSE, restart = 0, perturb = 1, 
          max.iter = Inf, maxp = Inf, optimized = TRUE)
-
 
 graphviz.plot(bn1)
 
@@ -35,17 +32,26 @@ score(bn1, df, type = "bde", iss = 3)
 score(bn1, df, type = "bde", iss = 5)
 
 
-im_sa_si <- c(2:20)
+im_sa_si <- c(1:2000)
 
-bnBD <- hc(df, start = NULL, score = "bde", iss=1, 
-           restart = 3, max.iter = Inf, 
-           maxp = Inf, optimized = TRUE)
+#bnBD <- hc(df, start = NULL, score = "bde", iss=2000, 
+#           restart = 3, max.iter = Inf, 
+#           maxp = Inf, optimized = TRUE)
+
+no_of_arcs <- integer(length=0)
+score <- integer(length=0)
 
 for (i in im_sa_si){
-  bnBD <- c(bnBD, hc(df, start = NULL, score = "bde", iss=i, 
-             restart = 3, perturb = 3, max.iter = Inf, 
-             maxp = Inf, optimized = TRUE))
+  bnBD <- hc(df, start = NULL, score = "bde", iss=i, 
+                   restart = 3, perturb = 3, max.iter = Inf,
+                   maxp = Inf, optimized = TRUE)
+  no_of_arcs = c(no_of_arcs, nrow(bnBD$arcs))
+  score = c(score, score(bnBD,df, type = "bde", iss=i))
 }
+
+
+plot(im_sa_si,no_of_arcs, xlab="Imaginary sample size", ylab="Number of arcs")
+plot(im_sa_si,score, type="l", xlab="Imaginary sample size", ylab="Score")
 
 
 
