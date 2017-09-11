26bb8eb1af311010c645aeb58b86f70aff84c9f9
Axel Rantil <axelrantil@Axels-MacBook-Pro.local>
2017-09-11 17:12:54 +0200

    tja

--- lab1/script.ada.R
+++ lab1/script.ada.R
@@ -1,5 +1,5 @@
 setwd("~/TDDE15/aml/lab1")
 installed.packages("bnlearn")
 source("http://bioconductor.org/biocLite.R")
-biocLite("graph", "RBGL", "Rgraphviz")
+biocLite(c("graph", "RBGL", "Rgraphviz")
 install.packages("gRain", dependencies=TRUE)
