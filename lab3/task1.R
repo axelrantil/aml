620c3372cb83803bd50965348a1ad4b1174bd8d6
Axel Rantil <axera283@student.liu.se>
2017-10-01 13:37:55 +0200

    task1 progress

---
+++ lab3/task1.R
@@ -0,0 +1,55 @@
+
+SquaredExpoKernel <- function(x1, x2, sigmaF=1, l=3){
+  n1 <- length(x1)
+  n2 <- length(x2)
+  K <- matrix(NA, n1, n2)
+  for (i in 1:n2){
+    K[,i] <- sigmaF^2*exp(-0.5*((x1-x2[i]/l)^2)) #Prior mean for f is zero
+  }
+  return(K)
+}
+
+# xStar: grid
+# x: datapunkter
+# y: target
+# hyperparam: vector with (sigmaf, l)
+posteriorGP <- function(x, y, xStar, hyperParam, sigmaNoise){
+  sigmaF <- hyperParam[[1]]
+  l <- hyperParam[[2]]
+  K <-SquaredExpoKernel(x, x, sigmaF, l)
+  L <- t(chol(K + sigmaNoise*diag(1, nrow(K),ncol(K))))
+  alpha <- solve(t(L), solve(L, y))
+  kStar <- SquaredExpoKernel(x, xStar, sigmaF, l)
+  MeanPosterior <- t(kStar)%*%alpha
+  v <- solve(L, kStar)
+  VarPosterior <- SquaredExpoKernel(xStar, xStar, sigmaF, l) - t(v)%*%v
+  return(list(MeanPosterior, VarPosterior))
+}
+
+xStar <- seq(-1,1,0.01)
+
+#meanvar <- posteriorGP(0.4, 0.719, xStar, c(1, 0.3), 0.1)
+
+sigmaF <- 1
+l <- 0.3
+sigmaNoise <- 0.1
+x <- 0.4
+y <- 0.719
+K <-SquaredExpoKernel(x, x, sigmaF, l)
+L <- t(chol(K + sigmaNoise*diag(1, nrow(K),ncol(K))))
+alpha <- solve(t(L), solve(L, y))
+kStar <- SquaredExpoKernel(x, xStar, sigmaF, l) #Normalisera f???
+MeanPosterior <- t(kStar)%*%alpha
+v <- solve(L, kStar)
+VarPosterior <- SquaredExpoKernel(xStar, xStar, sigmaF, l) - t(v)%*%v
+
+
+
+#mean <- meanvar[[1]]
+
+#var <- meanvar[[2]]
+#diag(var)
+
+# matplot(1:201, mean, type='l', col ="red", xlab="x", ylab="y")
+# lines(mean+1.96*sqrt(diag(var)), type="l", col="blue")
+# lines(mean-1.96*sqrt(diag(var)), type="l", col="green")
