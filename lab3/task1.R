89027a466fff2c987282770c350395cbd1a1a671
Axel Rantil <axera283@student.liu.se>
2017-10-03 10:04:18 +0200

    task 1 done

--- lab3/task1.R
+++ lab3/task1.R
@@ -9,6 +9,14 @@ SquaredExpKernel <- function(x1,x2,sigmaF=1,l=3){ #Implementera egen?
   return(K)
 }
 
+PlotLines <- function(x, y, grid, Mean, Variance){
+  UpperLimit <- Mean+1.96 * sqrt(Variance)
+  LowerLimit <- Mean-1.96 * sqrt(Variance)
+  matplot(grid, Mean, type='l', col ="red", xlab="x", ylab="y", ylim=c(-3, 3))
+  points(x, y, col="blue")
+  lines(grid, UpperLimit, type="l", col="green")
+  lines(grid, LowerLimit, type="l", col="green")
+}
 
 # xStar: grid
 # x: datapunkter
@@ -17,40 +25,85 @@ SquaredExpKernel <- function(x1,x2,sigmaF=1,l=3){ #Implementera egen?
 posteriorGP <- function(x, y, xStar, hyperParam, sigmaNoise){
   sigmaF <- hyperParam[[1]]
   l <- hyperParam[[2]]
-  K <-SquaredExpoKernel(x, x, sigmaF, l)
+  K <-SquaredExpKernel(x, x, sigmaF, l)
   L <- t(chol(K + sigmaNoise*diag(1, nrow(K),ncol(K))))
   alpha <- solve(t(L), solve(L, y))
-  kStar <- SquaredExpoKernel(x, xStar, sigmaF, l)
+  kStar <- SquaredExpKernel(x, xStar, sigmaF, l) 
   MeanPosterior <- t(kStar)%*%alpha
   v <- solve(L, kStar)
-  VarPosterior <- SquaredExpoKernel(xStar, xStar, sigmaF, l) - t(v)%*%v
+  VarPosterior <- SquaredExpKernel(xStar, xStar, sigmaF, l) - t(v)%*%v
   return(list(MeanPosterior, VarPosterior))
 }
 
 xStar <- seq(-1,1,0.01)
 
-#meanvar <- posteriorGP(0.4, 0.719, xStar, c(1, 0.3), 0.1)
+### 1b) ###
 
-sigmaF <- 1
-l <- 0.3
-sigmaNoise <- 0.1
 x <- 0.4
+
 y <- 0.719
-K <-SquaredExpKernel(x, x, sigmaF, l)
-L <- t(chol(K + sigmaNoise*diag(1, nrow(K),ncol(K))))
-alpha <- solve(t(L), solve(L, y))
-kStar <- SquaredExpKernel(x, xStar, sigmaF, l) 
-MeanPosterior <- t(kStar)%*%alpha # fstar normalisera?
-v <- solve(L, kStar)
-VarPosterior <- SquaredExpKernel(xStar, xStar, sigmaF, l) - t(v)%*%v
+
+posterior <- posteriorGP(x, y, xStar, c(1, 0.3), 0.1)
+
+MeanPosterior <- posterior[[1]]
+
+VarPosterior <- posterior[[2]]
+
+PlotLines(x, y, xStar, MeanPosterior, diag(VarPosterior))
+
+### 1c) ###
+
+x <- c(-0.6, 0.4)
+y <- c(-0.044, 0.719)
+
+posterior <- posteriorGP(x, y, xStar, c(1, 0.3), 0.1)
+
+MeanPosterior <- posterior[[1]]
+
+VarPosterior <- posterior[[2]]
+
+PlotLines(x, y, xStar, MeanPosterior, diag(VarPosterior))
+
+
+### 1d) ###
+
+x <- c(-1.0, -0.6, -0.2, 0.4, 0.8)
+y <- c(0.768, -0.044, -0.940, 0.719, -0.664)
+
+posterior <- posteriorGP(x, y, xStar, c(1, 0.3), 0.1)
+
+MeanPosterior <- posterior[[1]]
+
+VarPosterior <- posterior[[2]]
+
+PlotLines(x, y, xStar, MeanPosterior, diag(VarPosterior))
+
+### 1e) ###
+
+posterior <- posteriorGP(x, y, xStar, c(1, 1), 0.1)
+
+MeanPosterior <- posterior[[1]]
+
+VarPosterior <- posterior[[2]]
+
+PlotLines(x, y, xStar, MeanPosterior, diag(VarPosterior))
+
+### 2 ###
+install.packages("kernlab")
+library(kernlab)
+
+read.csv('https://github.com/STIMALiU/AdvMLCourse/raw/master/GaussianProcess/Code/TempTullinge.csv', header=TRUE,
+          sep=';')
+
+time <- c(1:2190)
+
+day <- c(1:2190 %% 365)
+day <- replace(day, day==0, 365)
+
+time <- time[seq(1,length(time), 5)]
+
+day <- day[seq(1,length(day), 5)]
+  
 
 
 
-#mean <- meanvar[[1]]
-
-#var <- meanvar[[2]]
-#diag(var)
-
-# matplot(1:201, mean, type='l', col ="red", xlab="x", ylab="y")
-# lines(mean+1.96*sqrt(diag(var)), type="l", col="blue")
-# lines(mean-1.96*sqrt(diag(var)), type="l", col="green")
